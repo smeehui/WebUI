d257b9579df874c253e614f87983b191ba70b7a8
smeehui <nguyenvanquanghuy99@gmail.com>
2022-11-01 08:10:11 +0700

    carosel

--- test.js
+++ test.js
@@ -1,15 +1,14 @@
-var arr = ["cam", "xoai", "chuoi", "quyt", "canh"];
-
-function search(inp) {
-    arr.forEach((fr) => {
-        let temp = "";
-        for (let i = 0; i < fr.length; i++) {
-            temp += fr[i];
-            if (!temp.localeCompare(inp)) {
-                console.log(fr);
-            }
-        }
-    });
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    let applCount = 0;
+    let orgCount = 0;
+    const calc = (fruits, startPoint) => {
+        const insideHome = fruits.filter((fruit, i) => {
+            fruits[i] += startPoint;
+            return fruits[i] >= s && fruits[i] <= t;
+        });
+        return insideHome.length;
+    };
+    console.log(calc(apples, a));
+    console.log(calc(oranges, b));
 }
-
-search("c");
+countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
